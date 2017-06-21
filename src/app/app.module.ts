d39fceb8d114ca376ddbf7b2ea49e3bf101f472e
Vinoth <lovelyvino@outlook.com>
2017-06-21 14:39:43 +0530

    Angular 2 - Nested Model
    dynamic form creation in ang2 using form builders

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent }   from './app.component';
+import { AddressComponent } from './address.component';
+
+@NgModule({
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, AddressComponent ],
+  bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
